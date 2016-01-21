f9d7a29694d9dbfcf8f39a5ab6adc61a60a2b6b3
Stanley Yeh <istanleyy@users.noreply.github.com>
2016-01-21 19:16:00 +0800

    Update read_test.py

--- read_test.py
+++ read_test.py
@@ -2,14 +2,25 @@ import time
 import serial
           
 ser = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-        )
+          port='/dev/ttyUSB0',
+          baudrate = 9600,
+          parity=serial.PARITY_NONE,
+          stopbits=serial.STOPBITS_ONE,
+          bytesize=serial.EIGHTBITS,
+          timeout=3
+          )
+          
+# Terminate test with Ctrl-C
+def end_read(signal,frame):
+    global do_test
+    print "Ctrl+C captured, ending test."
+    do_test = False
 
-while 1:
-  x=ser.readline()
-  print x
+# Hook SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+while do_test:
+  reader_response = ser.readline()
+  if reader_response != ""
+          hex_msg = " ".join("{:02x}".format(ord(c)) for c in reader_response)
+          print hex_msg
